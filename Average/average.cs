e0e739034ff51a71ef5cf175c986c88b0105683d
Margrét Unnarsdóttir <margretunnarsdottir@Margrets-MacBook-Pro.local>
2016-10-17 15:18:45 +0100

    first commit

---
+++ Average/average.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Average
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int value = 0;
+			string input;
+			double count = 0;
+			double sum = 0;
+			double average = 0;
+
+
+			while (value != -999)
+			{
+				Console.WriteLine("Enter a number");
+				input = Console.ReadLine();
+				value = int.Parse(input);
+
+				if (value != -999)
+				{
+					count += 1;
+					sum += value;
+
+					average = sum / count;
+				}
+			}
+
+			Console.WriteLine("The average is " + average);
+		}
+	}
+}
